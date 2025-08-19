984bd6412b8c0cced1ac3aaa8285ae41989fd714
alvinlaroya <alvinreggaelaroya@gmail.com>
2025-08-19 17:49:44 +0800

    fix: sidenav info mobile responsive

--- app/components/PersonalInformation.vue
+++ app/components/PersonalInformation.vue
@@ -23,20 +23,20 @@ const informations = [
 
 <template>
     <aside class="flex lg:sticky top-16 lg:h-[calc(100vh-4rem)] w-full lg:w-76">
-        <div class="p-4 w-full space-y-4 tracking-wide">
+        <div class="flex flex-row lg:flex-col space-x-4 p-4 w-full space-y-4 tracking-wide">
             <div class="flex justify-center items-center">
                 <NuxtImg src="/profile.webp" fit="cover" alt="profile-image" width="300" height="300"
                     class="w-[150px] rounded-full" />
             </div>
             <div>
-                <h2 class="text-center text-2xl mb-2 font-semibold">Alvin Laroya</h2>
+                <h2 class="text-left lg:text-center text-xl lg:text-2xl mb-2 font-semibold">Alvin Laroya</h2>
                 <div v-for="(info, i) in informations" :key="i" class="flex space-x-3 items-center mb-1.5">
                     <div class="flex">
                         <Icon :name="info.icon" size="1rem" />
                     </div>
                     <a :href="info.href" class="text-sm" :class="{ 'underline': info?.href }">{{ info.text }}</a>
                 </div>
-                <USeparator class="mt-3" />
+                <USeparator class="mt-3 hidden lg:block" />
             </div>
         </div>
     </aside>
