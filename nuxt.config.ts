f1791749523db8aac3a242e89e8e76b75cc01fda
alvinlaroya <alvinreggaelaroya@gmail.com>
2025-08-20 17:49:34 +0800

    config: fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,19 +18,20 @@ export default defineNuxtConfig({
           'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With'
         }
       }
-    },
-    runtimeConfig: {
-      public: {
-        adminKey: process.env.NUXT_PUBLIC_ADMIN_KEY,
-        n8nWebhookUrl: process.env.NUXT_PUBLIC_N8N_WEBHOOK_URL,
-      },
-      spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
-      spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-      spotifyRefreshToken: process.env.SPOTIFY_REFRESH_TOKEN,
-    },
-    supabase: {
-      url: process.env.SUPABASE_UR,
-      key: process.env.SUPABASE_KEY,
-      redirect: false,
     }
-  })
+  },
+  runtimeConfig: {
+    public: {
+      adminKey: process.env.NUXT_PUBLIC_ADMIN_KEY,
+      n8nWebhookUrl: process.env.NUXT_PUBLIC_N8N_WEBHOOK_URL,
+    },
+    spotifyClientId: process.env.SPOTIFY_CLIENT_ID,
+    spotifyClientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    spotifyRefreshToken: process.env.SPOTIFY_REFRESH_TOKEN,
+  },
+  supabase: {
+    url: process.env.SUPABASE_UR,
+    key: process.env.SUPABASE_KEY,
+    redirect: false,
+  }
+})
