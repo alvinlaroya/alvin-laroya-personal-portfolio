ee44e32e436433b4009b56ef0f60e1c281edffe1
alvinlaroya <alvinreggaelaroya@gmail.com>
2025-08-22 02:46:59 +0800

    chore: add placeholder in profile

--- app/components/PersonalInformation.vue
+++ app/components/PersonalInformation.vue
@@ -29,8 +29,8 @@ const informations = [
     <aside class="flex lg:sticky top-16 lg:h-[calc(100vh-4rem)] w-full lg:w-76">
         <div class="flex flex-row lg:flex-col space-x-4 p-4 w-full space-y-4 tracking-wide">
             <div class="flex justify-center items-center object-bottom">
-                <NuxtImg src="/profile2.webp" fit="cover" alt="profile-image" width="300" height="300"
-                    class="w-[140px] lg:w-[200px] rounded-full" />
+                <NuxtImg src="/profile2.webp" :placeholder="[200, 200]" fit="cover" alt="profile-image" width="300"
+                    height="300" class="w-[140px] lg:w-[200px] rounded-full" />
             </div>
             <div class="flex flex-col justify-center">
                 <h2 class="text-left lg:text-center text-xl lg:text-2xl mb-2 font-semibold">Alvin Laroya</h2>
@@ -50,7 +50,8 @@ const informations = [
                     </div>
                     <div class="mt-3">
                         <h2 class="text-sm text-gray-200">Hobbies:</h2>
-                        <span class="text-gray-400 text-xs">Coding • Playing Guitar • Singing • FPS Gaming • Coffee • Nature Travel</span>
+                        <span class="text-gray-400 text-xs">Coding • Playing Guitar • Singing • FPS Gaming • Coffee •
+                            Nature Travel</span>
                     </div>
                 </div>
                 <Connections class="hidden lg:block" />
