2ff71e07343fb5906a166b466f173feadb11bf15
alvinlaroya <alvinreggaelaroya@gmail.com>
2025-08-22 13:27:14 +0800

    fix

--- .netlify/functions-internal/server/chunks/build/Projects-styles-1.mjs-CdHIXZa5.mjs
+++
@@ -1,4 +0,0 @@
-const Projects_vue_vue_type_style_index_0_scoped_32b24623_lang = "@keyframes slideUp-32b24623{0%{opacity:0;transform:translateY(100%)}to{opacity:1;transform:translateY(0)}}@keyframes fadeInUp-32b24623{0%{opacity:0;transform:translateY(20px)}to{opacity:1;transform:translateY(0)}}@keyframes scaleIn-32b24623{0%{opacity:0;transform:scale(.8)}to{opacity:1;transform:scale(1)}}.slide-up[data-v-32b24623]{animation:slideUp-32b24623 .5s ease-out}.fade-in-up[data-v-32b24623]{animation:fadeInUp-32b24623 .6s ease-out}.scale-in[data-v-32b24623]{animation:scaleIn-32b24623 .4s ease-out}.card-overlay[data-v-32b24623]{background:#000c}";
-
-export { Projects_vue_vue_type_style_index_0_scoped_32b24623_lang as P };
-//# sourceMappingURL=Projects-styles-1.mjs-CdHIXZa5.mjs.map
--- .netlify/functions-internal/server/chunks/build/Projects-styles.shCikAeO.mjs
+++
@@ -1,6 +0,0 @@
-import { P as Projects_vue_vue_type_style_index_0_scoped_32b24623_lang } from './Projects-styles-1.mjs-CdHIXZa5.mjs';
-
-const ProjectsStyles_shCikAeO = [Projects_vue_vue_type_style_index_0_scoped_32b24623_lang, Projects_vue_vue_type_style_index_0_scoped_32b24623_lang];
-
-export { ProjectsStyles_shCikAeO as default };
-//# sourceMappingURL=Projects-styles.shCikAeO.mjs.map
