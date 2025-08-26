d8ad0a935d8a8d5ffd2deaf5d0f19c180c28a793
alvinlaroya <alvinreggaelaroya@gmail.com>
2025-08-26 18:36:01 +0800

    chore: update about me

--- app/components/AboutMe.vue
+++ app/components/AboutMe.vue
@@ -10,13 +10,7 @@ const yearsOfExperience = computed(() => {
     <div id="about" class="border border-gray-600 rounded-md p-4">
         <h2 class="text-xl font-semibold">About Me</h2>
         <p class="text-sm mt-3 text-gray-300 tracking-wide">
-            Results-driven Software Developer with {{ yearsOfExperience }}+ years of experience building modern,
-            high-performance
-            e-commerce platforms. Skilled in Vue/Nuxt, Svelte/SvelteKit, React/Next, and Tailwind CSS. Proven
-            track record in developing responsive, user-centric interfaces and integrating complex systems such
-            as store management tools. Experienced in modernizing legacy applications to improve performance,
-            usability, and efficiency. Passionate about creating innovative digital experiences that align with
-            business goals and drive measurable results.
+            Results-driven Software Developer with 6+ years of experience. Passionate about building scalable, user-focused applications that improve performance, enhance usability, and drive measurable business growth.
         </p>
     </div>
 </template>
